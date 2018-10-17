b148756696f9d8a2f1308c2dddc8f8381bc00b77
Shalom Ayidu <shalom.ayidu@andela.com>
2018-10-17 07:36:00 +0100

    WIP Prototype Pattern

---
+++ creational_patterns/prototype/prototype_pattern.rb
@@ -0,0 +1,38 @@
+class CartItem
+  attr_accessor :name, :price, :charged
+
+  def initialize(name, price)
+    @name = name
+    @price = price
+  end
+
+  def charge
+    @state = 'charged'
+  end
+end
+
+class Cart
+  attr_accessor :cart_item_collection
+
+  def initialize
+    @cart_item_collection = {}
+  end
+
+  def create_cart(cart_item)
+    cart_prototype = cart_item_collection[cart_item]
+    raise 'unsupported cart itme' unless cart_prototype
+
+    cart = cart_prototype.clone
+    cart.charge
+    cart
+  end
+end
+
+cart = Cart.new
+phone_prototype = CartItem.new('phone', 200)
+
+cart.cart_item_collection['phone'] = phone_prototype
+
+cart.create_cart('phone')
+
+# Can use ruby clone for implementation, for a shallow copy of the object.
